cc5a84639202011125ad84d517cf27a1580339be
MarcinNowak94 <marcin94nowak@gmail.com>
2023-11-14 23:17:08 +0100

    LAB_1_2 Done

--- LAB_1_2/app.py
+++ LAB_1_2/app.py
@@ -1,10 +1,14 @@
 #Source: https://pythonspot.com/flask-hello-world/
 from flask import Flask
 app = Flask(__name__)
+import sys
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    #as per: https://stackoverflow.com/questions/44405708/flask-doesnt-print-to-console
+    print("Użytkownik odwiedził stronę główną.", flush=True)
+    return "<h1>Hello World!</h1>"
+
 
 if __name__ == "__main__":
-    app.run(host="localhost",port=8000)
+    app.run()
--- LAB_1_2/startscript.sh
+++ LAB_1_2/startscript.sh
@@ -1,1 +1,3 @@
-python3 -m flask run --host localhost --port 8000
+#as per https://stackoverflow.com/questions/14258500/python-significance-of-u-option
+#-u parameter for unbuffered output (still does not work)
+python3 -u -m flask run --host 0.0.0.0 --port 8000 --debug
